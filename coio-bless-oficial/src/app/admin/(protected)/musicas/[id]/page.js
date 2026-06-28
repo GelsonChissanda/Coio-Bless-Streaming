7d2f86aeccdff07cb083aaf5aacb5199042758ad
Gelson Chissanda <gelsonchissanda@gmail.com>
2026-06-28 16:45:34 +0100

    update

--- coio-bless-oficial/src/app/admin/(protected)/musicas/[id]/page.js
+++ coio-bless-oficial/src/app/admin/(protected)/musicas/[id]/page.js
@@ -7,6 +7,7 @@ export const metadata = {
   robots: { index: false, follow: false },
 };
 
+
 export default async function EditarMusicaPage({ params }) {
   const { id } = await params;
   const [musica, albuns] = await Promise.all([
